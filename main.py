843f4c19effcb8cf796f9b423730ff42411a2f4b
Draichi <lucasdraichi@gmail.com>
2018-06-08 02:12:28 -0300

    running good

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sqlite3
 import pandas as pd 
 import dash
-from dash.dependencies import Output, Event
+from dash.dependencies import Output, Event, Input
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly
@@ -14,7 +14,9 @@ from collections import deque
 app = dash.Dash(__name__)
 app.layout = html.Div(
     [
-        dcc.Graph(id='live-graph', animate=True),
+        html.H2('Live Twitter Sentiment'),
+        dcc.Input(id='sentiment_term', value='o', type='text'),
+        dcc.Graph(id='live-graph', animate=False),
         dcc.Interval(
             id='graph-update',
             interval=1*1000
@@ -26,7 +28,8 @@ app.layout = html.Div(
     Output(
         'live-graph', 
         'figure'
-        ),
+    ),
+    [Input(component_id='sentiment_term', component_property='value')],
     events = [
         Event(
             'graph-update', 
@@ -34,21 +37,24 @@ app.layout = html.Div(
         )
     ]
 )
-def update_graph_scatter():
+def update_graph_scatter(sentiment_term):
     try:
         conn = sqlite3.connect('twitter.db')
         c = conn.cursor()
 
-        df = pd.read_sql("SELECT * FROM sentiment WHERE tweet LIKE '%trump%' ORDER BY unix DESC LIMIT 5000", conn)
+        df = pd.read_sql("SELECT * FROM sentiment WHERE tweet LIKE ? ORDER BY unix DESC LIMIT 200", conn ,params=('%' + sentiment_term + '%',))
 
         df.sort_values('unix', inplace=True)
 
         df['smoothed_sentiment'] = df['sentiment'].rolling(int(len(df)/5)).mean()
 
+        df['date'] = pd.to_datetime(df['unix'],unit='ms')
+        df.set_index('date', inplace=True)
+        df = df.resample('1min').mean()
         df.dropna(inplace=True)
 
-        x = df.unix.values[-100:]
-        y = df.smoothed_sentiment.values[-100:]
+        x = df.index
+        y = df.smoothed_sentiment
 
         data = plotly.graph_objs.Scatter(
             x=x,
@@ -70,8 +76,8 @@ def update_graph_scatter():
                         min(y),
                         max(y)
                     ]
-                    )
-                ,
+                ),
+                title = '{}'.format(sentiment_term)
             )
         }
     except Exception as e:
